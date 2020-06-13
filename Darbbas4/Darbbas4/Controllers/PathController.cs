aafadde1cc849cea31fe279af143d77e7465944f
Ruslan <s035180@ad.viko.lt>
2020-06-13 15:56:20 +0300

    asd

--- Darbbas4/Darbbas4/Controllers/PathController.cs
+++ Darbbas4/Darbbas4/Controllers/PathController.cs
@@ -1,5 +1,6 @@
 ﻿using Darbbas4.Rep;
 using Newtonsoft.Json.Linq;
+using RestSharp;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -19,12 +20,13 @@ namespace Darbbas4.Controllers
         }
 
         // GET: api/Path/5
-        public JObject Get(string pav)
+        public JObject Get(string pav, string pav2)
         {
-            PlaceRep pr = new PlaceRep();
-            JObject j = new JObject();
-            j = pr.Action(pav, "Naujamiestis");
-            return j;
+            var client = new RestClient("https://www.mapquestapi.com/directions/v2/route?key=IUioukh6TOaFypNnaHuJfuKM0xDknNti&from=" + pav + "&to=" + pav2 + "&outFormat=json&ambiguities=ignore&routeType=fastest&doReverseGeocode=false&enhancedNarrative=false&avoidTimedConditions=false");
+            var request = new RestRequest(Method.GET);
+            IRestResponse response = client.Execute(request);
+            JObject json = JObject.Parse(response.Content);
+            return json;
 
         }
 
