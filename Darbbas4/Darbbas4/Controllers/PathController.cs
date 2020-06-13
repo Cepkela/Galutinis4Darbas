20339b452134a058de4553bc07b79f71aeff47a0
Cepkis <r.sulnis@gmail.com>
2020-06-13 15:45:00 +0300

    s

---
+++ Darbbas4/Darbbas4/Controllers/PathController.cs
@@ -0,0 +1,47 @@
+﻿using Darbbas4.Rep;
+using Newtonsoft.Json.Linq;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace Darbbas4.Controllers
+{
+    public class PathController : ApiController
+    {
+        // GET: api/Path
+        [HttpGet]
+        public IEnumerable<string> Get()
+        {
+            return new string[] { "value1", "value2" };
+        }
+
+        // GET: api/Path/5
+        public JObject Get(string pav)
+        {
+            PlaceRep pr = new PlaceRep();
+            JObject j = new JObject();
+            j = pr.Action(pav, "Naujamiestis");
+            return j;
+
+        }
+
+        // POST: api/Path
+        [HttpPost]
+        public void Post([FromBody] string value)
+        {
+        }
+
+        // PUT: api/Path/5
+        public void Put(int id, [FromBody] string value)
+        {
+        }
+
+        // DELETE: api/ApiWithActions/5
+        public void Delete(int id)
+        {
+        }
+    }
+}
---
+++ Darbbas4/Darbbas4/Models/Place.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Darbbas4.Models
+{
+    public class Place
+    {
+        public string startPoint { get; set; }
+        public string startPointID { get; set; }
+        public string endPoint { get; set; }
+        public string endPointID { get; set; }
+        public double distance { get; set; }
+
+        public ArrayList streets = new ArrayList();
+
+        public Place()
+        {
+
+        }
+        public Place(string startPoint, string endPoint, double distance, string startPointID, string endPointID)
+        {
+            this.startPoint = startPoint;
+            this.endPoint = endPoint;
+            this.distance = distance;
+            this.startPointID = startPointID;
+            this.endPointID = endPointID;
+        }
+    }
+}
