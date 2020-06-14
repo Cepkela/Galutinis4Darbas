84f7c77e3f764abb31cddbf0368272e320027d3c
Cepkis <r.sulnis@gmail.com>
2020-06-14 13:30:37 +0300

    Streets

--- Darbbas4/Darbbas4/Controllers/PathController.cs
+++ Darbbas4/Darbbas4/Controllers/PathController.cs
@@ -1,8 +1,11 @@
-﻿using Darbbas4.Rep;
+﻿using Darbbas4.Models;
+using Darbbas4.Rep;
+using Newtonsoft.Json;
 using Newtonsoft.Json.Linq;
 using RestSharp;
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Net;
 using System.Net.Http;
@@ -19,7 +22,7 @@ namespace Darbbas4.Controllers
         }
 
         // GET: api/Path/5
-        public JObject Get(string pav, string pav2)
+        public Place Get(string pav, string pav2)
         {
             var client = new RestClient("https://www.mapquestapi.com/directions/v2/route?key=IUioukh6TOaFypNnaHuJfuKM0xDknNti&from=" + pav + "&to=" + pav2 + "&outFormat=json&ambiguities=ignore&routeType=fastest&doReverseGeocode=false&enhancedNarrative=false&avoidTimedConditions=false");
             var request = new RestRequest(Method.GET);
@@ -32,15 +35,31 @@ namespace Darbbas4.Controllers
             p.itemList.Add(data.streets);
             p.startPoint = pav;
             p.endPoint = pav2;
+            using (StreamWriter sw = new StreamWriter((@"C:\Users\Cepkis\Source\Repos\Galutinis4Darbas\Darbbas4\Darbbas4\rez.txt"), true))
+                {
+                sw.WriteLine(data);
+                }
+            StreamReader sr = new StreamReader(@"C:\Users\Cepkis\Source\Repos\Galutinis4Darbas\Darbbas4\Darbbas4\rez.txt");
+            string line;
+            char[] chars = { ']',',' };
+            while ((line = sr.ReadLine()) != null)
+            {
+                if (line.Contains("streets"))
+                {
+                    while (!line.Contains("]"))
+                    {
+                        line = sr.ReadLine();
+                        p.itemList.Add(line);
+                    }
 
+                }
+            }
             if (!string.IsNullOrEmpty(jsona))
             {
-                string b = jsona.Split(',')[0];
+                string b = jsona.Split(',')[100];
                 if (b == "streets")
                     p.itemList.Add(b);
             }
-            //s
-
             return p;
         }
 
